5fef2bcf2843f8e26456d93c7b7f4ba574278aeb
Sergeisbq <ss_b@icloud.com>
2023-03-27 18:25:58 +0300

    7

--- Week2/Day2/Smth.py
+++ Week2/Day2/Smth.py
@@ -1,5 +1,6 @@
 p_top = input("Type some toppings you'd to add to pizza: ")
-while p_top != 'quit':
-    p_top
-    if p_top != 'quit':
+while True:
+    if p_top == 'quit':
         break
+    else:
+        print(f"{p_top} is added")
