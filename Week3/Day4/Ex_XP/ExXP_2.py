1ffdf3dfe7937353d41aee52fa3078dfcf28e8ae
Sergeisbq <ss_b@icloud.com>
2023-04-15 00:03:29 +0300

    7

--- Week3/Day4/Ex_XP/ExXP_2.py
+++ Week3/Day4/Ex_XP/ExXP_2.py
@@ -1,6 +1,6 @@
 import json
 
-sampleJson = { 
+sampleJson = '''{ 
    "company":{ 
       "employee":{ 
          "name":"emma",
@@ -10,4 +10,20 @@ sampleJson = {
          }
       }
    }
-}
+}'''
+
+
+some_str = json.loads(sampleJson)
+print(some_str['company']['employee']['payable']['salary'])
+
+some_str['company']['employee']['payable']['salary'] += 700
+print(some_str['company']['employee']['payable']['salary'])
+
+some_str['company']['employee']['birth_date'] = '12/12/2021'
+print(some_str)
+
+sampleJson_new = json.dumps(some_str, indent = 2, sort_keys=True)
+print (sampleJson_new)
+
+with open("ExXP.json",'w') as file:
+    json.dump(some_str, file, indent = 2)
