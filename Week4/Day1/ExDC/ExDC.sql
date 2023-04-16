eb03869ffe486327c02b2181015f723347f47f5f
Sergeisbq <ss_b@icloud.com>
2023-04-16 14:59:37 +0300

    3

---
+++ Week4/Day1/ExDC/ExDC.sql
@@ -0,0 +1,21 @@
+-- create table actors(
+--  actor_id serial primary key,
+--  first_name varchar (50) not null,
+--  last_name varchar (100) not null,
+--  age date not null,
+--  number_oscars smallint not null
+-- )
+
+-- insert into actors (first_name, last_name, age, number_oscars)
+-- values
+-- ('Matt','Damon','08/10/1970', 5),
+-- ('George','Clooney','06/05/1961', 2);
+
+-- insert into actors (first_name, last_name, age, number_oscars)
+-- values
+-- ('Kevin','Bacon','07/08/1958', 0),
+-- ('Christian','Bale','01/30/1974', 1);
+
+-- select * from actors
+
+-- select count(*) from actors
