8c89a104e6f9f147b7835823e0be328bf4660df7
Sergeisbq <ss_b@icloud.com>
2023-04-16 14:41:30 +0300

    1

---
+++ Week4/Day1/ExXP/ExXP.sql
@@ -0,0 +1,41 @@
+-- create table items (
+-- id serial primary key,
+-- item_name varchar(20),
+-- price int
+-- ); 
+
+
+-- create table customers (
+-- id serial primary key,
+-- first_name varchar(10),
+-- last_name varchar(10)
+-- ); 
+
+
+-- insert into items(item_name, price) 
+-- values
+-- ('Small Desk', 100),
+-- ('Large desk', 300),
+-- ('Fan', 80); 
+
+
+-- insert into customers(first_name, last_name) 
+-- values
+-- ('Greg', 'Jones'),
+-- ('Sandra', 'Jones'),
+-- ('Scott', 'Scott'),
+-- ('Trevor', 'Green'),
+-- ('Melanie', 'Johnson'); 
+
+-- select * from items
+-- select * from customers
+
+-- select * from items where price > 80
+-- select * from items where price <= 300
+
+-- select * from customers where last_name = 'Smith'
+
+-- select * from customers where last_name = 'Jones'
+
+-- select * from customers where first_name != 'Scott'
+
---
+++ Week4/Day1/ExXp+/ExXP+.sql
@@ -0,0 +1,40 @@
+-- create table students (
+-- id serial primary key,
+-- last_name varchar(10),
+-- first_name varchar(10),	
+-- birth_date date
+-- ); 
+
+
+-- insert into students(last_name, first_name, birth_date) 
+-- values
+-- ('Benichou', 'Marc', '1998-11-02'),
+-- ('Cohen', 'Yoan', '2010-12-03'),
+-- ('Benichou', 'Lea', '1987-07-27'),
+-- ('Dux', 'Amelia', '1996-07-04'),
+-- ('Grez', 'David', '2003-06-14'),
+-- ('Simpson', 'Omer', '1980-10-03'); 
+
+
+-- insert into students(last_name, first_name, birth_date) 
+-- values
+-- ('Boiko', 'Sergei', '1990-12-16'); 
+
+
+-- select * from students 
+
+-- select * from students where last_name = 'Benichou' and first_name = 'Marc'
+
+-- select * from students where last_name = 'Benichou' or first_name = 'Marc'
+
+-- select * from students where last_name = 'Benichou' or first_name = 'Marc'
+
+-- select * from students where (first_name ~* '[a]') is true
+
+-- select first_name from students where first_name like 'A%';
+
+-- select first_name from students where first_name like '%a';
+
+-- select first_name from students where first_name like '%a_';
+
+-- select * from students where id = 1 or id = 3;
