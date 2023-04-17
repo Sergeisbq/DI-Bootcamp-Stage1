41fb3970f48d5383807358450afdbfcbfcdccf0e
Sergeisbq <ss_b@icloud.com>
2023-04-17 12:40:11 +0300

    3

---
+++ Week4/Day2/ExDC/Ex_DC.sql
@@ -0,0 +1,45 @@
+-- CREATE TABLE FirstTab (
+-- id integer, 
+-- name VARCHAR(10)
+-- );
+
+
+-- INSERT INTO FirstTab VALUES
+-- (5,'Pawan'),
+-- (6,'Sharlee'),
+-- (7,'Krish'),
+-- (NULL,'Avtaar');
+
+-- SELECT * FROM FirstTab
+
+-- CREATE TABLE SecondTab (
+-- id integer
+-- );
+
+-- INSERT INTO SecondTab 
+-- VALUES
+-- (5),
+-- (NULL);
+
+-- SELECT * FROM SecondTab
+
+
+-- SELECT COUNT(*) 
+--     FROM FirstTab AS ft WHERE ft.id NOT IN ( SELECT id FROM SecondTab WHERE id IS NULL )
+-- answer:	
+-- nothing
+
+-- SELECT COUNT(*) 
+--     FROM FirstTab AS ft WHERE ft.id NOT IN ( SELECT id FROM SecondTab WHERE id = 5 )
+-- answer:	
+-- 2 (6,7)
+
+-- SELECT COUNT(*) 
+--     FROM FirstTab AS ft WHERE ft.id NOT IN ( SELECT id FROM SecondTab )
+-- answer:	
+-- nothing
+
+-- SELECT COUNT(*) 
+--     FROM FirstTab AS ft WHERE ft.id NOT IN ( SELECT id FROM SecondTab WHERE id IS NOT NULL )
+-- answer:	
+-- 2 (6,7)	
