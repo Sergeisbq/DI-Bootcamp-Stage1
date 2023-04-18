ef3cf744677a4256b93b6c05421416e85edb2535
Sergeisbq <ss_b@icloud.com>
2023-04-18 23:08:53 +0300

    7

---
+++ Week4/Day3/Examples/Smth.sql
@@ -0,0 +1,12 @@
+-- create table smth (
+-- id serial not Null primary key,
+-- name varchar(10)
+-- );
+
+-- insert into smth (name)
+-- values 
+-- ('Alex'),
+-- ('Bob'),
+-- ('Jack');
+
+select * from smth
