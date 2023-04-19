3b58b7c497806ad9a31ef1bfe4395cd97f9d99c3
Sergeisbq <ss_b@icloud.com>
2023-04-20 00:17:05 +0300

    3

--- Week4/ExXP_test.py
+++ Week4/ExXP_test.py
@@ -1,18 +1,18 @@
-def delete():
-        a = all()
-        print(a)
-        b = input("Type the item'd to delete: ")
-        if b == a:
-            query = f"delete from cafe_menu where name = '{b}';"
-            with connection.cursor() as cursor:
-                cursor.execute(query)
-                connection.commit()
-                connection.close()
-                print(f'{b} has been successfully deleted!')
-        else:
-            print('There is no such item!')
+# def delete():
+#         a = all()
+#         print(a)
+#         b = input("Type the item'd to delete: ")
+#         if b == a:
+#             query = f"delete from cafe_menu where name = '{b}';"
+#             with connection.cursor() as cursor:
+#                 cursor.execute(query)
+#                 connection.commit()
+#                 connection.close()
+#                 print(f'{b} has been successfully deleted!')
+#         else:
+#             print('There is no such item!')
 
 
-list_of_items = []
+# list_of_items = []
 
 
