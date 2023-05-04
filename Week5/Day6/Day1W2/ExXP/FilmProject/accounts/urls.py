80d0cd0e3686c7134207ef31c97db36d1c42c729
Sergeisbq <ss_b@icloud.com>
2023-05-04 21:38:15 +0300

    1

--- Week5/Day6/Day1W2/ExXP/FilmProject/accounts/urls.py
+++ Week5/Day6/Day1W2/ExXP/FilmProject/accounts/urls.py
@@ -14,12 +14,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+
+from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
+from .views import ProfileView, SignUpView
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("", include('films.urls')),
-    path("", include('accounts.urls')),
+    path('accounts/signup/', SignUpView.as_view(), name='signup'),
+    path('accounts/login/',  LoginView.as_view(template_name='login.html'), name='login'),
+    path('accounts/logout/', LogoutView.as_view(template_name='logout.html'), name='logout'),
+    path('accounts/profile/', ProfileView.as_view(), name='profile'),
 
-]
+]    
--- Week5/Day6/Day1W2/ExXP/FilmProject/accounts/views.py
+++ Week5/Day6/Day1W2/ExXP/FilmProject/accounts/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.views.generic import DetailView, CreateView, UpdateView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
+from .models import UserProfile
+from .forms import ProfileForm, CustomSingUpForm
+from django.contrib.auth.mixins import LoginRequiredMixin
 # Create your views here.
+
+# class ProfileView(DetailView):
+#     model = UserProfile
+#     template_name = 'profile.html'
+#     context_object_name = 'profile'
+
+
+
+class SignUpView(CreateView):
+    form_class = CustomSingUpForm
+    success_url = reverse_lazy('login')
+    template_name = 'signup.html'
+
+
+
+class ProfileView(LoginRequiredMixin, DetailView): # LoginRequiredMixin - to access users that already login, DetailView - we need to see only about 1 user
+    model = UserProfile #python bull-in model
+    template_name = 'profile.html' # template_name = profile.html
+    context_object_name = 'profile_context' # take object to template
+
+    def get_object(self, queryset=None): # take loggedin user
+ 
+        return self.request.user #self.query.set - access to user
