0bd13c90287b358fd9dc427556aeb7d63c597bc9
Sergeisbq <ss_b@icloud.com>
2023-05-07 12:28:40 +0300

    5

--- Week7/ExampleDay1/weather_report/weather_report/urls.py
+++ Week7/ExampleDay1/weather_report/weather_report/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from weatherapp.views import ReportView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
+    path('api/weathers/', ReportView.as_view(), name='weather-list'),
 ]
--- Week7/ExampleDay1/weather_report/weatherapp/views.py
+++ Week7/ExampleDay1/weather_report/weatherapp/views.py
@@ -1,4 +1,24 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+# from django.http import HttpResponse, JsonResponse
+# from rest_framework.parsers import JSONParser
+from .models import Report
+from .serializers import ReportSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import (IsAdminUser, 
+                                        IsAuthenticated, 
+                                        AllowAny)
 
 # Create your views here.
+
+
+class ReportView(APIView):
+
+    permission_classes = (AllowAny)
+
+    def get(self, request, *args, **kwargs):
+        queryset = Report.objects.all()
+        serializer = ReportSerializer(queryset, many=True)
+        return Response(serializer.data)
 
