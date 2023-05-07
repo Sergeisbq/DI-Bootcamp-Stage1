a97e847add717043a28f4df17fbd08fc84b1e193
Sergeisbq <ss_b@icloud.com>
2023-05-07 13:59:24 +0300

    5

--- Week7/Day1/polls_rest/polls_rest/urls.py
+++ Week7/Day1/polls_rest/polls_rest/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api/posts/', PostView.as_view(), name='post-list'),
+    path('api/posts/<int:pk>', PostView.as_view(), name='post'),
 ]
