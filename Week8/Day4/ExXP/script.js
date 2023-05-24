35704c7a136fd0397898559a15a149537af26daf
Sergeisbq <ss_b@icloud.com>
2023-05-24 13:30:57 +0300

    1

---
+++ Week8/Day4/ExXP/script.js
@@ -0,0 +1,94 @@
+
+// // -----1-----
+// const person = {
+//     name: 'John Doe',
+//     age: 25,
+//     location: {
+//         country: 'Canada',
+//         city: 'Vancouver',
+//         coordinates: [49.2827, -123.1207]
+//     }
+// }
+
+// const {name, location: {country, city, coordinates: [lat, lng]}} = person;
+
+// console.log(`I am ${name} from ${city}, ${country}. Latitude(${lat}), Longitude(${lng})`);
+// // Answer - I am John Doe from Vancouver, Canada. Latitude(49.2827), Longitude(-123.1207).
+
+
+// // -----2-----
+// function displayStudentInfo(objUser){
+//     const {first, last } = objUser;
+//     console.log(`Your full name is ${first} ${last}`);
+// }
+
+// displayStudentInfo({first: 'Elie', last:'Schoppik'});
+
+
+// // -----3-----
+// const users = {user1: 18273, user2: 92833, user3: 90315};
+// const newArr = [];
+// Object.entries(users).forEach(([key, value], i) => {
+//         newArr.push([key, value * 2]);
+//     });
+// console.log(newArr);
+
+
+// // -----4-----
+// class Person {
+//     constructor(name) {
+//       this.name = name;
+//     }
+//   }
+  
+//   const member = new Person('John');
+//   console.log(typeof member); 
+// // Answer - Object.
+
+
+// // -----5-----
+// class Dog {
+//     constructor(name) {
+//       this.name = name;
+//     }
+// };
+
+// // This one will successfully extend the Dog class:
+// class Labrador extends Dog {
+//     constructor(name, size) {
+//       super(name);
+//       this.size = size;
+//     }
+// };
+
+
+// // -----6-----
+const object1 = { number: 5 }; 
+const object2 = object1; 
+const object3 = object2; 
+const object4 = { number: 5};
+
+object1.number = 4;
+console.log(object2.number) // Answer - 4
+console.log(object3.number) // Answer - 4
+console.log(object4.number) // Answer - 5
+
+class Animal {
+    constructor(name, type, color) {
+        this.name = name;
+        this.type = type;
+        this.color = color;
+    }
+}
+
+class Mamal extends Animal {
+    constructor(name, type, color) {
+        super(name, type, color);
+    }
+    sound(param) {
+        console.log(`${param} I'm a ${this.type}, named ${this.name} and I'm ${this.color} `);
+    }
+}
+
+const farmerCow = new Mamal('Lily', 'cow', 'brown and white');
+farmerCow.sound('Moooo')
---
+++ Week8/Day4/Example/script.js
@@ -0,0 +1,264 @@
+// let arr1 = [2,4,4,1];
+
+// function bCC(arr) {
+//     let amount = 0;
+//     arr.forEach((item) => {
+//         if (item >= 4)
+//         amount += 1;
+//         return amount
+//     });
+//     console.log(amount); 
+// }
+
+// bCC(arr1);
+
+
+// let arr = [2,4,4,1];
+
+
+// function birthdayCakeCandels(arr) {
+//     let maxNum = Math.max(...arr);
+//     let filter = arr.filter(value => {
+//         return value === maxNum
+//     })
+//     // console.log(filter);
+//     return filter.length;
+// }
+
+// let how_many = birthdayCakeCandels(arr);
+// console.log(how_many);
+
+
+// let arr_f = [2,5,3,7,2,3,6,8,6];
+// let arr_n = []
+// function smalestDist(arr) {
+//     for (i in arr) {
+//         arr_n.push(arr[i]);
+//         i += 1;
+//         if (i+1 !== i) {
+//             arr_n.push(arr[i])
+//         }
+//     }
+//     console.log(arr_n); 
+
+//     }
+
+// smalestDist(arr_f)
+
+
+
+// let arr = [2,5,3,7,2,3,6,8,6];
+
+// function minimumDistances(a) {
+//     let distances = [];
+//     for (let i = 0; i < a.length; i++) {
+//         if (a.indexOf(a[i]) !== a.lastIndexOf(a[i])) {
+//             let minDistance = a.lastIndexOf(a[i]) - a.indexOf(a[i]);
+//             distances.push(minDistance);
+//         }
+//     }
+//     return (distances.length === 0) ? -1 : distances.sort()[0]
+// }
+
+// let smallest = minimumDistances(arr)
+// console.log(smallest);
+
+
+
+// Array Destructuring
+
+// let arr = [2,5,3,7,2,3,6,8,6];
+
+// // const a = arr[0];
+// // const b = arr[1];
+// // ===
+
+// const [a, b, c] = arr;
+
+// console.log(b);
+
+
+// Array Spreading
+
+// let a = 1;
+// let b = 2;
+// let arr2 = [5,6,7];
+// let arr3 = [a, ...arr2, b];
+
+// console.log(arr3);
+
+
+// Array Rest parameters
+
+// let arr = [2,5,3,7,2,3,6,8,6];
+// const [a, b, c, ...rest] = arr;
+// console.log(a, b, c, rest);
+
+// function x([a,b, ...rest]) {
+//     console.log(a);
+//     console.log(b);
+//     console.log(rest);
+// }
+
+// x(arr)
+
+
+
+// // Object method
+
+// const population = {
+//     tokyo: 37833000,
+//     delhi: 24953000,
+//     shanghai: 22991000,
+// }
+
+// for (i in population) {
+//     console.log(i, population[i]);
+// } 
+
+// // Object.keys
+// console.log(Object.keys(population));
+// Object.keys(population).forEach((item, i) => {
+//     console.log(item);
+// });
+
+// // Object.values
+// console.log(Object.values(population));
+// Object.values(population).forEach((item, i) => {
+//     console.log(item);
+// });
+
+// // Object.entries
+// console.log(Object.entries(population));
+// Object.entries(population).forEach((item, i) => {
+//     console.log(item);
+// });
+// // ===
+// console.log(Object.entries(population));
+// Object.entries(population).forEach(([key, value], i) => { // === [key, value] = [a, b]
+//     console.log(key, value);
+// });
+
+
+// Object Distructuring
+
+// const population = {
+//     tokyo: 37833000,
+//     delhi: 24953000,
+//     shanghai: 22991000,
+// }
+
+// const {shanghai, tokyo, delhi} = population;
+// console.log(tokyo, delhi, shanghai);
+
+
+// Object Spreading
+
+// const population = {
+//     tokyo: 37833000,
+//     delhi: 24953000,
+//     shanghai: 22991000,
+// }
+
+// let obj2 = {
+//     tokyo: 4444,
+// }
+
+// const obj3 = {...population, tokyo: 0, telaviv: 22367000}
+// console.log(obj3);
+
+// const obj3 = {...population, ...obj2} // if keys of objects are equal, new object gets value from the last object
+// console.log(obj3);
+
+// let {tokyo} = population;
+// let population_tokyo = tokyo;
+// let {tokyo} = obj3;
+// console.log(population_tokyo, tokyo);
+
+
+
+// Classes
+
+// class NameOfClass {
+//     constructor(param1, param2) {
+//         this.name = 'John';
+//         this.surname = param1;
+//         this.birthday = param2;
+//     }
+//     getName() {
+//         return this.name + this.surname + this.birthday
+//     }
+//     setA(param){
+//         this.name = param
+//     }
+// }
+
+// let abc = new NameOfClass('c', 'd');
+// let bca = new NameOfClass('r', 't');
+// bca.setA('z')
+// console.log(abc.getName());
+// console.log(bca.getName());
+
+
+class Animal {
+    #name
+    constructor(name) {
+        this.#name = name;
+    }
+    speak() {
+        console.log(`${this.#name} makes a noise`);
+    }
+}
+
+class Dog extends Animal {
+    constructor(name, color) {
+        super(name);
+        this.color = color;
+    }
+    speak() {
+        console.log(`${this.name} ${this.color} bark`);
+    }
+    getColor() {
+        return this.color
+    }
+}
+
+class Cat extends Animal {
+    constructor(name, color) {
+        super(name);
+    }
+    speak() {
+        console.log(`${this.name} meow`);
+    }
+    getColor() {
+        return this.color
+    }
+}
+
+class Frenchi extends Dog {
+    constructor(name, color, type) {
+        super(name, color);
+        this.type = type;
+    }
+    getName() {
+        return this.name
+    }
+}
+
+const dog = new Dog('Buddy', 'black');
+dog.speak()
+
+const dog1 = new Dog('Archi', 'white');
+dog1.speak()
+
+const fr = new Frenchi ('Rex', 'Brown', 'Frenchi');
+fr.getColor();
+fr.speak();
+fr.name = 'Rexie1';
+console.log(fr.name);
+
+// const dog = new Animal('Dog');
+// dog.speak()
+
+// const cat = new Animal('Cat');
+// cat.speak()
