62085efb2f2db3cccc5f9fc91ab2a67db12ee244
Sergeisbq <ss_b@icloud.com>
2023-06-04 14:45:00 +0300

    5

---
+++ Week10/Day1/ExXP/npm-node/main.js
@@ -0,0 +1,16 @@
+//-----1-----
+
+// const a = 5;
+// const b = 10;
+
+// console.log(`Hello you are ${a + b}`); // node main.js
+
+
+//-----3-----
+
+const a = 5;
+const b = 10;
+let x;
+const c = 6;
+x = a + b;
+console.log(x + c);
