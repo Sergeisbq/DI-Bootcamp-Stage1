4f35354abe4136cfe1514e899c6f0d0f1bd68e08
Sergeisbq <ss_b@icloud.com>
2023-06-11 16:16:31 +0300

    7

---
+++ Week11/Day1/ExDC/my-app/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import DemoCarousel from './Carousel.js';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <DemoCarousel />
+      </header>
+    </div>
+  );
+}
+
+export default App;
