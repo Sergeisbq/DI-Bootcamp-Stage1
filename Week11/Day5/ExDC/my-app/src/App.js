0996432ab691a7a81f8875695c2642bdfb8d24ec
Sergeisbq <ss_b@icloud.com>
2023-06-17 20:57:20 +0300

    5

--- Week11/Day5/ExDC/my-app/src/App.js
+++ Week11/Day5/ExDC/my-app/src/App.js
@@ -1,13 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
-import ToDo from './components/ToDo';
+import React from 'react';
+import heroes from './superheroes.json'
 
-function App() {
-  return (
-    <div className="App">
-      <ToDo />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      heroes: heroes.superheroes,
+      clcHeroes: [],
+      score: 0,
+      topScore: 0
+    }
+  }
+
+  handleClick = (nameH) => {
+    this.setState((state) => {
+      const newScore = state.clcHeroes.includes(nameH) ? 0 : state.score + 1;
+      const clcHeroes = state.clcHeroes.includes(nameH) ? [] : [...this.state.clcHeroes, nameH];
+      return {
+        heroes: [...this.state.heroes].sort((a, b) => (Math.random() > 0.5 ? -1 : 1)),
+        clcHeroes: clcHeroes,
+        score: newScore,
+        topScore: Math.max(newScore, state.topScore)
+      }
+    });
+  };
+  
+
+  render() {
+    return (
+      <div className="App">
+        <nav>
+          <h1>Superheroes Memory Game</h1>
+          <div>Score: {this.state.score}</div>
+          <div>Top Score: {this.state.topScore}</div>
+        </nav>
+        <h1>Get points by clicking on an image but don't click on any more than once!</h1>
+        <div style={{display: 'grid', gridTemplateColumns: 'repeat(auto-fill, minmax(300px, 1fr))', gap: "3rem"}}>
+          {this.state.heroes.map((item) => {
+            return(
+              <div key={item.id} onClick={() => this.handleClick(item.name)}>
+                <h4>{item.name}</h4>
+                <p>{item.occupation}</p>
+                <img src={item.image} width={"200px"}></img>
+              </div>
+            )
+          })}
+        </div>
+      </div>
+    );
+  }
+
 }
 
 export default App;
