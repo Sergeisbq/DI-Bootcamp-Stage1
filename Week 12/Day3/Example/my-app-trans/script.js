ab1d0f443530014513fe8344a6c9fd068c47c97c
Sergeisbq <ss_b@icloud.com>
2023-06-20 12:00:49 +0300

    5

---
+++ Week 12/Day3/Example/my-app-trans/script.js
@@ -0,0 +1,7 @@
+localStorage.setItem('dog', 'frenchi');
+
+const car = localStorage.getItem('dog');
+
+localStorage.clear()
+
+console.log(car);
--- Week 12/Day3/Example/my-app-trans/src/redux/reducer.js
+++ Week 12/Day3/Example/my-app-trans/src/redux/reducer.js
@@ -1,7 +1,8 @@
 import { INSERT, UPDATE, DELETE, UPDATE_INDEX } from './actions';
+import { addToLocalStorage, getFromLocalStorage } from '../utils/storage';
 
 const initState = {
-    list: [],
+    list: getFromLocalStorage('transactions'),
     currentIndex: -1
 }
 
@@ -9,14 +10,17 @@ export const reducer = (state=initState, action={}) => {
     switch (action.type) {
         case INSERT:
             state.list.push(action.payload)
+            addToLocalStorage('transactions', [...state.list])
             return {...state, list: [...state.list], currentIndex: -1}
         case UPDATE:
             state.list[state.currentIndex] = action.payload
+            addToLocalStorage('transactions', [...state.list])
             return {...state, list:[...state.list], currentIndex: -1}
         case UPDATE_INDEX:
             return {...state, currentIndex: action.payload}
         case DELETE:
             state.list.splice(action.payload, 1)
+            addToLocalStorage('transactions', [...state.list])
             return {...state, list:[...state.list], currentIndex: -1}
         default:
             return {...state}
